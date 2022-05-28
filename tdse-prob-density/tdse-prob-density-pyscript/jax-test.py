e95f522b01f9213123a1e86704b51121da227b26
KBC <kcollins9@ucmerced>
2022-05-28 13:14:06 -0700

    Added Ptyhon script to test JAX on computing resource

---
+++ tdse-prob-density/tdse-prob-density-pyscript/jax-test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import jax
+import jax.numpy as jnp
+
+def sqfn(x):
+    return x**2
+
+gradsqfn = jax.grad(sqfn)
+
+vec = jnp.array([1, 3, 7])
+
+print('sqfn:', sqfn(vec))
+
+print('gradsqfn:', gradsqfn(vec))
