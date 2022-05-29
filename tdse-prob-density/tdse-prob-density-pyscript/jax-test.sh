95f394e54d68028a735cf12706e6889883135e83
KBC <none@private.com>
2022-05-28 22:29:07 -0700

    updates

--- tdse-prob-density/tdse-prob-density-pyscript/jax-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -p gpu
-#SBATCH --job-name=jax-gpu-test
-#SBATCH --output=./jax-gpu-test.out
-#SBATCH --error=./jax-gpu-test.err
-#SBATCH --time=00:05:00
-#SBATCH --gres=gpu:2
-
-python jax-test.py
