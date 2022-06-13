957a49430973392b241cd28164b6148c94f979d4
KBC <none@private.com>
2022-06-13 11:07:11 -0700

    adds

---
+++ tdse-prob-density/tdse-prob-density-pyscript/tdse-prob-density-forward/loadcmpenv.py
@@ -0,0 +1,8 @@
+#!\usr\bin\env python
+import pathlib
+import numpy as np
+
+inputpath = pathlib.Path('./v0')
+cmpenv = np.load(inputpath / 'cmpenv.npy', allow_pickle=True)
+print(type(cmpenv))
+print(cmpenv)
