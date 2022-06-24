a35a122fafb69e1153e583c16116c7a668c6f9c3
KBC <kcollins9@ucmerced>
2022-06-24 10:08:52 -0700

    Reorganized files

--- tdse-amp-squared/tdse-amp-squared-main/tdse-amp-squared-main-script/tdse-amp-squared-main-script-forward-inverse/loadcmpenv.py
+++ tdse-amp-squared/tdse-amp-squared-main/tdse-amp-squared-main-script/tdse-amp-squared-main-script-forward-inverse/loadcmpenv.py
@@ -2,7 +2,7 @@
 import pathlib
 import numpy as np
 
-inputpath = pathlib.Path('./v0')
+inputpath = pathlib.Path('v0')
 cmpenv = np.load(inputpath / 'cmpenv.npy', allow_pickle=True)
 print(type(cmpenv))
 print(cmpenv)
