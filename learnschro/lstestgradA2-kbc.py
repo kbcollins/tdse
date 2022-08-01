3a40e95dd628982fa9070d2c659a856732474d8b
KBC <kcollins9@ucmerced>
2022-08-01 12:45:17 -0700

    Updates lstestgradA2-kbc.py

--- learnschro/lstestgradA2-kbc.py
+++ learnschro/lstestgradA2-kbc.py
@@ -226,7 +226,6 @@ def compgradhess(x, realic):
                 ttm = corrpraj - corrajpr
                 # ddJrealreal[r, s] += jnp.real(t1 + alpha * tp @ ttp)
                 ddJrealimag[r, s] += jnp.imag(t2 + alpha * tp @ ttm)
-                print('-->jnp.imag(t2 + alpha * tp @ ttm):', jnp.imag(t2 + alpha * tp @ ttm))
                 # ddJimagreal[r, s] += jnp.real(t1 + alpha * tm @ ttp)
                 # ddJimagimag[r, s] += jnp.imag(t2 + alpha * tm @ ttm)
 
@@ -319,9 +318,9 @@ for i in range(numruns):
     print('-->Error gradJ:', jnp.linalg.norm(jaxdJ - gradJ))
 
     print('-->Shape jaxHJ:', jaxHJ.shape)
-    print('-->jaxHJ[2*nmax+1:, 2*nmax+1:]:', jaxHJ[2*nmax+1:, 2*nmax+1:])
+    print('-->jaxHJ[:2*nmax+1, 2*nmax+1:]:', jaxHJ[:2*nmax+1, 2*nmax+1:])
     print('-->Shape HJ:', HJ.shape)
-    print('-->HJ[2*nmax+1:, 2*nmax+1:]:', HJ[2*nmax+1:, 2*nmax+1:])
+    print('-->HJ[:2*nmax+1, 2*nmax+1:]:', HJ[:2*nmax+1, 2*nmax+1:])
     print('-->Diff HJ:', HJ - jaxHJ)
     print('-->Error HJ:', jnp.linalg.norm(HJ - jaxHJ))
 
